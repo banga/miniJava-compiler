4b86e63081e8454d43bc5ad8499cd04584defc12
Shrey Banga <banga.shrey@gmail.com>
2012-01-28 07:56:53 -0500

    Now print the offending line and location

--- src/Assignment 1/src/miniJava/Compiler.java
+++ src/Assignment 1/src/miniJava/Compiler.java
@@ -1,28 +1,48 @@
 package miniJava;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 import miniJava.SyntacticAnalyzer.Parser;
+import miniJava.SyntacticAnalyzer.SourcePosition;
 import miniJava.SyntacticAnalyzer.SyntaxErrorException;
 
 public class Compiler {
-  public static void main(String[] args) {
-	  if(args.length < 1) {
-		  System.out.println("Usage: java miniJava.Compiler <filename>");
-		  System.exit(4);
-	  }
+	private static void printOffendingLine(String fileName, SourcePosition position) {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(fileName));
+			for(int i = 1; i < position.line; i++)
+				in.readLine();
+			System.out.println(in.readLine().replace('\t', ' '));
+			for(int i = 1; i < position.column; i++)
+				System.out.print(' ');
+			System.out.println('^');
+		} catch (IOException e) {
+		}
+	}
+	
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Usage: java miniJava.Compiler <filename>");
+			System.exit(4);
+		}
 
-	  try {
-		  Parser parser = new Parser(new FileInputStream(args[0]));
-		  parser.parseProgram();
-	  } catch(SyntaxErrorException e) {
-		  e.printStackTrace();
-		  System.out.println(e);
-		  System.exit(4);
-	  } catch(IOException e) {
-		  System.out.println("Error opening " + args[0]);
-		  System.exit(4);
-	  }
-  }
+		try {
+			Parser parser = new Parser(new FileInputStream(args[0]));
+			parser.parseProgram();
+		} catch (SyntaxErrorException e) {
+			//e.printStackTrace();
+			if (e.token != null && e.token.position != null) {
+				printOffendingLine(args[0], e.token.position);
+			}
+
+			System.out.println(e.getMessage());
+			System.exit(4);
+		} catch (IOException e) {
+			System.out.println("Error opening " + args[0]);
+			System.exit(4);
+		}
+	}
 }
