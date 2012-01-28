5c1664b2c64c9dc8850592f055c7d0c1fdde0b04
Shrey Banga <banga.shrey@gmail.com>
2012-01-28 00:13:00 -0500

    Don't need to store position separately, token already has it

--- src/Assignment 1/src/miniJava/SyntacticAnalyzer/SyntaxErrorException.java
+++ src/Assignment 1/src/miniJava/SyntacticAnalyzer/SyntaxErrorException.java
@@ -3,21 +3,18 @@ package miniJava.SyntacticAnalyzer;
 @SuppressWarnings("serial")
 public class SyntaxErrorException extends Exception {
 	public Token token = null;
-	public SourcePosition position = null;
 	public String extra = "Syntax error";
 
-	public SyntaxErrorException(Token token, SourcePosition position) {
+	public SyntaxErrorException(Token token) {
 		this.token = token;
-		this.position = position;
 	}
 
-	public SyntaxErrorException(Token token, SourcePosition position, String extra) {
+	public SyntaxErrorException(Token token, String extra) {
 		this.token = token;
-		this.position = position;
 		this.extra = extra;
 	}
-	
+
 	public String toString() {
-		return "Syntax error: " + extra + " at " + token.spelling  + ", " + position;
+		return "Syntax error: " + extra + " at " + token.spelling + ", " + token.position;
 	}
 }
