24fa9e93c105235a07359a8b0c5fa63b9af76853
Shrey Banga <banga.shrey@gmail.com>
2012-02-18 22:35:19 -0500

    Adding test files

---
+++ src/project/tests/1/fail166.java
@@ -0,0 +1,8 @@
+// PA1 parse assign fail
+class Test {
+
+    void p(int a) {
+	a.v[] = 4;
+    }
+}
+
---
+++ src/project/tests/1/pass156.java
@@ -0,0 +1,7 @@
+// PA1 parse Stmt this ref pass
+class LegalStmt {
+   void main () {
+       this(3,4);
+       this[3] = 4; 
+   }
+}
