2152a5712f73b0354c17972c2e0d42239ff43d69
Shrey Banga <banga.shrey@gmail.com>
2012-02-25 21:06:51 -0500

    Changed the target directory to the dropbox directory

--- src/project/scp2classroom.cmd
+++ src/project/scp2classroom.cmd
@@ -1,2 +1,2 @@
 @echo OFF
-c:\~\linux\pscp -i c:\users\Shrey\Documents\UNC\CS\ssh\banga.ppk -r src/miniJava banga@snapper.cs.unc.edu:/afs/unc/proj/courses/comp520/Submit/banga/pa1/
+c:\~\linux\pscp -i c:\users\Shrey\Documents\UNC\CS\ssh\banga.ppk -r src/miniJava banga@classroom.cs.unc.edu:/home/banga/alexis/
