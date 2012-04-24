4f027ef6762986dffebbaa5040c4fa3c6a09bb19
Shrey Banga <banga.shrey@gmail.com>
2012-04-24 15:35:57 -0400

    Adding class object displacement to classRuntimeEntity

--- src/project/src/miniJava/CodeGenerator/ClassRuntimeEntity.java
+++ src/project/src/miniJava/CodeGenerator/ClassRuntimeEntity.java
@@ -4,7 +4,9 @@ public class ClassRuntimeEntity {
 
 	public ClassRuntimeEntity(int size) {
 		this.size = size;
+		this.classObjectDisplacement = -1;
 	}
 
 	public int size;
+	public int classObjectDisplacement;
 }
